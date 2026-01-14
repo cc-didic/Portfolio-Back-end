8303a6405290bbaa674dc7277dbcf789c883c2e9
cc-didic <cc.cedric.caron@gmail.com>
2026-01-14 14:55:30 +0100

    Ajout des routes pour les méthodes new, show, edit et delete dans le contrôleur Project.

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -4,16 +4,33 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ProjectController extends AbstractController
+#[Route('api/project', name: 'app_api_project_')]
+class ProjectController extends AbstractController
 {
-    #[Route('/project', name: 'app_project')]
-    public function index(): JsonResponse
+    #[Route(name: 'new', methods: 'POST')]
+    public function new(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProjectController.php',
-        ]);
+
+    }
+
+    #[Route('/', name: 'show', methods: 'GET')]
+    public function show(): Response
+    {
+
+    }
+
+    #[Route('/', name: 'edit', methods: 'PUT')]
+    public function edit(): Response
+    {
+
+    }
+
+    #[Route('/', name: 'delete', methods: 'DELETE')]
+    public function delete(): Response
+    {
+
     }
 }
